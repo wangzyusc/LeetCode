a4cfefc529a2a9ecaefe0eb0fa9a63276f96ff63
wangzyusc <wangzhiy@usc.edu>
2017-01-29 01:01:28 -0800

    No 31 first solution

---
+++ 31_NextPermutation/Solution.cpp
@@ -0,0 +1,43 @@
+#include "../IOLib.hpp"
+
+using namespace std;
+
+/*
+ * With reference to:
+ * http://fisherlei.blogspot.com/2012/12/leetcode-next-permutation.html
+ */
+
+class Solution {
+public:
+  void nextPermutation(vector<int>& nums) {
+    int length = nums.size();
+    if(length < 2) return;
+    int partition, change;
+    int i;
+    for(i = length - 2; i >= 0; i--){
+      if(nums[i] < nums[i+1]){
+	partition = nums[i];
+	break;
+      }
+    }
+    if(i >= 0){
+      for(int j = length - 1; j > i; j--){
+	if(nums[j] > nums[i]){
+	  nums[i] = nums[j];
+	  nums[j] = partition;
+	  break;
+	}
+      }
+    }
+    sort(nums.begin()+1+i, nums.end());
+  }
+};
+
+int main(){
+    vector<int> vec;
+    readVector(vec);
+    Solution solution;
+    solution.nextPermutation(vec);
+    viewVector(vec);
+    return 0;
+}
