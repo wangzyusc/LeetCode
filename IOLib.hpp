9a3e44c68d4f7d0799b09b1a3ec8a6a5d304cacd
wangzyusc <wangzhiy@usc.edu>
2017-01-31 01:58:48 -0800

    IOLib: Added Binary Trees functions: reading, building(error) and (in-order) traversing

--- IOLib.hpp
+++ IOLib.hpp
@@ -11,6 +11,14 @@ struct ListNode {
     ListNode(int x) : val(x), next(NULL) {}
 };
 
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    //TreeNode() : val(0), left(NULL), right(NULL) {}
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
 void readVector(vector<int>& input){
 	string line;
 	int num;
@@ -20,6 +28,16 @@ void readVector(vector<int>& input){
   	while(stream >> num){
     	input.push_back(num);
   	}
+}
+
+void viewVector(vector<int>& vec, int length = -1){
+    if(length == -1) length = vec.size();
+    cout << "[";
+    for(int i = 0; i < length; i++){
+        cout << vec[i];
+    if(i < length - 1) cout << ", ";
+    }
+    cout << "]" << endl;
 }
 
 ListNode* readLinkedList(){
@@ -51,19 +69,61 @@ void viewLinkedList(ListNode* head){
     cout << "]" << endl;
 }
 
-void viewVector(vector<int>& vec, int length = -1){
-    if(length == -1) length = vec.size();
-    cout << "[";
-    for(int i = 0; i < length; i++){
-        cout << vec[i];
-	if(i < length - 1) cout << ", ";
-    }
-    cout << "]" << endl;
-}
-
 int readInt(){
     cout << "Input a number: ";
     int res;
     cin >> res;
     return res;
+}
+
+/*
+This version of buildBinaryTree is with problem,
+it cannot successfully build a tree. Cannot link
+child nodes to parent node.
+*/
+void buildBinaryTree(TreeNode* root, vector<int>& nums, int idx){
+    int length = nums.size();
+    if(idx >= length) return;
+    if(idx == 0) root = new TreeNode(nums[idx]);
+    int leftidx = 2 * idx + 1, rightidx = leftidx + 1;
+    if(leftidx < length){
+        root->left = new TreeNode(nums[leftidx]);
+        buildBinaryTree(root->left, nums, leftidx);
+    }
+    if(rightidx < length){
+        root->right = new TreeNode(nums[rightidx]);
+        buildBinaryTree(root->right, nums, rightidx);
+    }
+}
+
+TreeNode* readBinaryTree(){
+    vector<int> nums;
+    string line;
+    int num;
+    cout << "Input number elements in binary tree: " << endl;
+    getline(cin, line);
+    istringstream stream(line);
+    while(stream >> num){
+        nums.push_back(num);
+    }
+    viewVector(nums);
+    TreeNode* root = NULL;
+    buildBinaryTree(root, nums, 0);
+    return root;
+}
+
+void InOrderTraverse(TreeNode* node){
+    if(node == NULL){
+        cout << "null ";
+        return;
+    }
+    if(node->left != NULL) InOrderTraverse(node->left);
+    cout << node->val << " ";
+    if(node->right != NULL) InOrderTraverse(node->right);
+}
+
+void viewBinaryTree(TreeNode* root){
+    cout << "In-Order Traverse of tree: " << endl;
+    InOrderTraverse(root);
+    cout << endl;
 }
