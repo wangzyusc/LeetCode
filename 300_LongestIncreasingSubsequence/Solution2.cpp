60b7f12401d468d12d6c72b7754133ab8cfcf76b
wangzyusc <wangzhiy@usc.edu>
2017-09-29 19:44:26 -0700

    No 300 O(n2) and O(nlg(n)) solutions

---
+++ 300_LongestIncreasingSubsequence/Solution2.cpp
@@ -0,0 +1,33 @@
+#include "../IOLib.hpp"
+
+/*
+ * Time complexity: O(nlg(n))
+ * Space complexity: O(n)
+ * Roughly understood the principle, but not very well.
+ */
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int> length(nums.size(), 1);
+        int res = 0;
+        for(int i = 0; i < nums.size(); i++){
+            int mlen = 0;
+            for(int j = 0; j < i; j++){
+                if(nums[j] < nums[i]){
+                    mlen = max(mlen, length[j]);
+                }
+            }
+            length[i] = mlen + 1;
+            res = max(res, length[i]);
+        }
+        return res;
+    }
+};
+
+int main(void){
+    vector<int> nums;
+    readVector(nums);
+    Solution sol;
+    cout << "res = " << sol.lengthOfLIS(nums) << endl;
+    return 0;
+}
