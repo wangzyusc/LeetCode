004f94df0c33cddfb66bbb872b0f51beeb2d7b4e
wangzyusc <wangzhiy@usc.edu>
2018-01-20 16:05:26 -0800

    No 26 easier version

--- 26_RemoveDupFromSortedArray/Solution.cpp
+++ 26_RemoveDupFromSortedArray/Solution.cpp
@@ -19,6 +19,20 @@ public:
   }
 };
 
+/* More concise version */
+class Solution2 {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if(nums.empty()) return 0;
+        int j = 0;
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] != nums[j])
+                swap(nums[i], nums[++j]);
+        }
+        return j + 1;
+    }
+};
+
 int main(void){
   vector<int> input;
   readVector(input);
