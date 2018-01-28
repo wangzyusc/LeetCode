497aa552e3ed8c5f218e5a8fb54b7dc7923c57e8
wangzyusc <wangzhiy@usc.edu>
2018-01-28 15:17:34 -0800

    No 11 more concise version

--- 11_ContainerWithMostWater/Solution.cpp
+++ 11_ContainerWithMostWater/Solution.cpp
@@ -17,6 +17,23 @@ public:
   }
 };
 
+/* A more concise version */
+class Solution2 {
+public:
+    int maxArea(vector<int>& height) {
+        int water = 0;
+        int l = 0, r = height.size() - 1;
+        while(l < r){
+            water = max(water, (r - l) * min(height[l], height[r]));
+            if(height[l] < height[r])
+                l++;
+            else
+                r--;
+        }
+        return water;
+    }
+};
+
 int main(void){
   vector<int> height;
   readVector(height);
